4416a52a0e6301ec0537bc0749442983f3d2dc07
bycamp <bycamp0604@naver.com>
2025-03-19 09:12:32 +0900

    파일 추가 03/18

---
+++ vue-mock-server/src/components/DataBindingList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>제품명</th>
+          <th>가격</th>
+          <th>카테고리</th>
+          <th>배송료</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="i" v-for="(product, i) in productList">
+          <td>{{ product.product_name }}</td>
+          <td>{{ product.price }}</td>
+          <td>{{ product.category }}</td>
+          <td>{{ product.delivery_price }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      productList: []
+    }
+  },
+  created () {
+    this.getList()
+  },
+  methods: {
+    async getList () {
+      this.productList = await this.$api(
+        'https://af1845bd-f415-4445-b4fb-17e429cf4bf1.mock.pstmn.io/list',
+        'get'
+      )
+    }
+  }
+}
+</script>
+<style scoped>
+table {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 60%;
+  margin: 0 auto;
+}
+td,
+th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+</style>
